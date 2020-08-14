e15dd53c9c0e2db7300117ccaaf46724a8902a38
Jhon <jhon@mac-pro.local>
2020-08-13 22:31:15 -0500

    Home instructions and login style changed

--- src/components/account/login.jsx
+++ src/components/account/login.jsx
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 import { Grid, Container, Button, TextField } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import logo from "../../images/logo192.png";
+import logo from "../../images/logo2.png";
 import { useUser } from "../../context/user-provider.jsx";
 
 const useStyles = makeStyles(theme => ({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        alignItems:"center",
+        margin:"auto",
+        width:"30%",
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        padding: '0px',
+        marginTop: "100px",
+    },
     paper: {
         padding: theme.spacing(2),
-        textAlign: "center",
-        color: theme.palette.text.secondary
+        textAlign: "center"
     }
 }));
+
 
 export default function LoginForm() {
     const classes = useStyles();
@@ -33,28 +44,31 @@ export default function LoginForm() {
 
     return (
         <Container>
-            <form>
-                <img src={logo} style={{ marginTop: "100px" }} alt="Payments" ></img>
-                <Grid item className={classes.paper}>
-                    <TextField
-                        label="UserName"
-                        name="username"
-                        value={form.username}
-                        onChange={updateField}>
-                    </TextField>
-                </Grid>
-                <Grid item className={classes.paper}>
-                    <TextField
-                        label="Password"
-                        name="password"
-                        type="password"
-                        value={form.password}
-                        onChange={updateField}>
-                    </TextField>
-                </Grid>
-                <Grid item className={classes.paper}>
-                    <Button color="primary" onClick={handleLogin}>Sign In</Button>
-                </Grid>
+            <form className={classes.root}>
+                <div style={{padding:"50px 10px"}}>
+                    <h1 style={{ color:"#f50057"}}>Payments app</h1>
+                    {/* <img src={logo} style={{ width:"25%" }} alt="Payments" ></img> */}
+                    <Grid item className={classes.paper}>
+                        <TextField
+                            label="UserName"
+                            name="username"
+                            value={form.username}
+                            onChange={updateField}>
+                        </TextField>
+                    </Grid>
+                    <Grid item className={classes.paper}>
+                        <TextField
+                            label="Password"
+                            name="password"
+                            type="password"
+                            value={form.password}
+                            onChange={updateField}>
+                        </TextField>
+                    </Grid>
+                    <Grid item className={classes.paper}>
+                        <Button style={{ color: "#f50057"}} onClick={handleLogin}>Sign In</Button>
+                    </Grid>
+                </div>
             </form>
         </Container >
     );
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,9 +1,80 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+    },
+    nested: {
+        paddingLeft: theme.spacing(4),
+    },
+}));
 
 export default function Home() {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(true);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
     return (
         <div>
-            <h1>Home content</h1>
+            <h1 style={{ color: "#f50057" }}>Payment process</h1>
+
+            <List
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+                className={classes.root}>
+                <ListItem button>
+                    <ListItemIcon>
+                        <SendIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Choose the tab products" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <SendIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Select a product from the listgrid" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <SendIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="A modal with the details of the product will appear" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <SendIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="On the checkout page please fill the card details you can use the stripe dummy number cards" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <SendIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Process the payment" />
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <SendIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="The result page with the payment data will be shown" />
+                </ListItem> </List>
         </div>
     );
 }
